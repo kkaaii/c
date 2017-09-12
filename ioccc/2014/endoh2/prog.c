c69143d1ba606906b12889a6dd6abe58b5345020
zhaokai115 <zhaokai115@gmail.com>
2017-09-12 07:01:16 -0400

    expand macro

--- ioccc/2014/endoh2/prog.c
+++ ioccc/2014/endoh2/prog.c
@@ -3,7 +3,7 @@
 #define TA	q=
 #define CG	r=
 
-#define Y(z)	d;d=c++%2<1?x=x*4+z,c%8>5?x=x?p(1,x),0:x:0:0;d=
+#define Y(z)	d;if (c++%2<1){x=x*4+z;if (c%8>5 && x)p(1,x);}d=
 
 #define A	Y(0)
 #define C	Y(1)
@@ -26,15 +26,23 @@ int main()
     if (f)
         f();
     else {
-        for (puts("#include"    "\40\"prog.c\"\n\n     \101T"+0);
-            d = !d ? x = (x = getchar()) < 0 ? 0 : x, 8 * 8 : d,
-            TA++c%8,
-            TA(1+7*q-q*q)/3,
+        puts("#include \"prog.c\"\n\n     AT");
+        for (; d = !d ? x = (x = getchar()) < 0 ? 0 : x, 8 * 8 : d,
+            q=++c%8,
+            q=(1+7*q-q*q)/3,
             r=c*15-c*c-36,
             p(r < 0 ? !q+4 : r/6+!q+4, 32),
             q||x; c%=16)
-            q ? p(1, "ACGT"[x/d&3]), p(q,126), p(1, "TGCA"[x/d&3]), d/=4, p(001,10) : puts(c%8?"CG":"TA");
-            puts("GC");
+            if (q) {
+                p(1, "ACGT"[x/d&3]);
+                p(q,'~');
+                p(1, "TGCA"[x/d&3]);
+                d/=4;
+                p(1,'\n');
+            } else {
+                puts(c%8?"CG":"TA");
+            }
+        puts("GC");
     }
     return 0;
 }
