e7bd9856594601d6846e897f2e436c8820c5cf63
zhaokai115 <zhaokai115@gmail.com>
2017-11-17 03:21:36 -0500

    fix Wrong Answer

--- poj/3181 Dollar Dayz/3181.c
+++ poj/3181 Dollar Dayz/3181.c
@@ -6,46 +6,51 @@
 #include <stdio.h>
 #include <string.h>
 
-#define	N	1000	/* number od dollars ranges [1..N] */
-#define	K	100		/* cost of tools ranges [1..K] */
+#define	N	1000	/* number of dollars ranges [1..N] */
+#define	K	100	/* cost of tools ranges [1..K] */
 
-int n;
-int k;
+#define	MOD	1000000000000000000LL
+
+long long b[N + 1][K + 1];
+long long a[N + 1][K + 1];
 
 int main(void)
 {
+	int	n, k;
+	int	i, j, m;
+
 	scanf("%d%d", &n, &k);
-	printf("%d\n", dp());
+
+	memset(b, 0, sizeof b);
+	memset(a, 0, sizeof a);
+
+	for (i = 1; i <= n; ++i) {
+		b[i][1] = 0;
+		a[i][1] = 1;
+
+		for (j = 2; j <= k; ++j) {
+			b[i][j] = b[i][j - 1];
+			a[i][j] = a[i][j - 1];
+			if (j > i) continue;
+
+			for (m = j; m <= i; m += j) {
+				if (m == i) {
+					++a[i][j];
+				} else {
+					b[i][j] += b[i - m][j - 1];
+					a[i][j] += a[i - m][j - 1];
+				}
+				b[i][j] += a[i][j] / MOD;
+				a[i][j]  = a[i][j] % MOD;
+			}
+		}
+	}
+
+	if (b[n][k]) {
+		printf("%lld", b[n][k]);
+	}
+	printf("%lld\n", a[n][k]);
 
 	return 0;
-}
-
-int f[N + 1];
-
-void unbounded_knapsack(int f[], int c, int w, int v)
-{
-	int i;
-
-	for (i = w; i <= c; ++i) {
-		int t = f[i - w] + v;
-		if (f[c] < t)
-			f[c] = t;
-	}
-}
-
-int dp(void)
-{
-	int w;
-	int c;
-
-	memset(f, 0, sizeof f);
-	f[0] = 1;
-
-	for (w = 1; w <= k; ++w) {
-		for (c = w; c <= n; ++c)
-			f[c] += f[c - w];
-	}
-
-	return f[n];
 }
 
