3289aeca784510fd73b254fdc8d9e472be2f771b
zhaokai115 <zhaokai115@gmail.com>
2018-08-26 02:59:43 -0400

    Refactoring: Replace Nested Conditional with Guard Clauses - refactored

--- Refactoring/ReplaceNestedConditionalWithGuardClauses.cpp
+++ Refactoring/ReplaceNestedConditionalWithGuardClauses.cpp
@@ -1,29 +1,15 @@
 double getPayAmount()
 {
-	double result;
-
-	if (_isDead) result = deadAmount();
-	else {
-		if (_isSeparated) result = separatedAmount();
-		else {
-			if (_isRetired) result = retiredAmount();
-			else result = normalAmount();
-		}
-	}
-
-	return result;
+	if (_isDead) return deadAmount();
+	if (_isSeparated) return separatedAmount();
+	if (_isRetired) return retiredAmount();
+	return normalAmount();
 }
 
 double getAdjustedCapital()
 {
-	double result = 0.0;
-
-	if (_capital > 0.0) {
-		if (_intRate > 0.0 && _duration > 0.0) {
-			result = (_income / _duration) * ADJ_FACTOR;
-		}
-	}
-
-	return result;
+	if (_capital <= 0.0) return 0.0;
+	if (_intRate <= 0.0 || _duration <= 0.0) return 0.0;
+	return (_income / _duration) * ADJ_FACTOR;
 }
 
