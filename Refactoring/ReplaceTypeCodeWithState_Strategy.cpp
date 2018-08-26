21aff029a06b404fde73cf61f104caf85ce2c7aa
zhaokai115 <zhaokai115@gmail.com>
2018-08-25 23:40:31 -0400

    Refactoring: ReplaceTypeCodeWithState_Strategy - refactored

--- Refactoring/ReplaceTypeCodeWithState_Strategy.cpp
+++ Refactoring/ReplaceTypeCodeWithState_Strategy.cpp
@@ -1,7 +1,72 @@
-class Employee {
-	int _type;
-	enum {ENGINEER = 0, SALESMAN = 1, MANAGER = 2};
+class EmployeeType {
 public:
-	Employee(int type): _type(type) {}
+	enum {ENGINEER = 0, SALESMAN = 1, MANAGER = 2};
+
+	virtual ~EmployeeType() {}
+	virtual int getTypeCode() const = 0;
+
+	static EmployeeType *newType(int code);
 };
+
+class Employee {
+	EmployeeType *_type;
+public:
+	Employee(int type): _type(0) {
+		setType(type);
+	}
+
+public:
+	int getType() const {
+		return _type->getTypeCode();
+	}
+
+	void setType(int code) {
+		if (0 != _type) {
+			delete _type;
+			_type = 0;
+		}
+
+		_type = EmployeeType::newType(code);
+	}
+};
+
+class Engineer: public EmployeeType {
+public:
+	virtual int getTypeCode() const {
+		return EmployeeType::ENGINEER;
+	}
+};
+
+class Salesman: public EmployeeType {
+public:
+	virtual int getTypeCode() const {
+		return EmployeeType::SALESMAN;
+	}
+};
+
+class Manager: public EmployeeType {
+public:
+	virtual int getTypeCode() const {
+		return EmployeeType::MANAGER;
+	}
+};
+
+EmployeeType *EmployeeType::newType(int code)
+{
+	switch (code) {
+	case ENGINEER:
+		return new Engineer();
+
+	case SALESMAN:
+		return new Salesman();
+
+	case MANAGER:
+		return new Manager();
+
+	default:
+		break;
+	}
+
+	return 0;
+}
 
