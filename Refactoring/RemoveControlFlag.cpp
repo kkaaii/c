56a277f4b245ff5a2c321f64178d2b5fcc38366c
zhaokai115 <zhaokai115@gmail.com>
2018-08-26 02:33:15 -0400

    Refactoring: Remove Control Flag - refactored

--- Refactoring/RemoveControlFlag.cpp
+++ Refactoring/RemoveControlFlag.cpp
@@ -1,40 +1,38 @@
 void checkSecurity(String people[])
 {
-	bool found = false;
-
 	for (int i = 0; i < people.length(); ++i) {
-		if (!found) {
-			if (people[i].equals("Don")) {
-				sendAlert();
-				found = true;
-			}
+		if (people[i].equals("Don")) {
+			sendAlert();
+			break;
+		}
 
-			if (people[i].equals("John")) {
-				sendAlert();
-				found = true;
-			}
+		if (people[i].equals("John")) {
+			sendAlert();
+			break;
 		}
 	}
 }
 
-void checkSecurity(String people[])
+String foundMiscreant(String people[])
 {
-	String found = "";
-
 	for (int i = 0; i < people.length(); ++i) {
-		if (found.equals("")) {
-			if (people[i].equals("Don")) {
-				sendAlert();
-				found = "Don";
-			}
+		if (people[i].equals("Don")) {
+			sendAlert();
+			return "Don";
+		}
 
-			if (people[i].equals("John")) {
-				sendAlert();
-				found = "John";
-			}
+		if (people[i].equals("John")) {
+			sendAlert();
+			return "John";
 		}
 	}
 
+	return "";
+}
+
+void checkSecurity(String people[])
+{
+	String found = foundMiscreant(people);
 	someLaterCode(found);
 }
 
