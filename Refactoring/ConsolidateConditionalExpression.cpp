5585605e918e66ca958cf7d1c3efc43b128c0b11
zhaokai115 <zhaokai115@gmail.com>
2018-08-26 02:11:14 -0400

    Refactoring: Consolidate Conditional Expression - refactored

--- Refactoring/ConsolidateConditionalExpression.cpp
+++ Refactoring/ConsolidateConditionalExpression.cpp
@@ -1,12 +1,11 @@
+bool isNotEligibleForDisability() {
+	return (_seniority < 2) || (_monthsDisabled > 12) || (_isPartTime);
+}
+
 double disabilityAmount() {
-	if (_seniority < 2) return 0;
-	if (_monthsDisabled > 12) return 0;
-	if (_isPartTime) return 0;
+	if (isNotEligibleForDisability()) return 0;
 	// TODO: compiute the disability amount
 }
 
-if (onVacation())
-	if (lengthOfService() > 10)
-		return 1;
-return 0.5;
+return (onVacation() && lengthOfService() > 10) ? 1 : 0.5;
 
