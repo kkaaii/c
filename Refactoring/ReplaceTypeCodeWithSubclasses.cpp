1e4e3e2d9ce98ed30780b5be980c53e3b5af4c9d
zhaokai115 <zhaokai115@gmail.com>
2018-08-25 23:17:08 -0400

    Refactoring: ReplaceTypeCodeWithSubclasses - refactored

--- Refactoring/ReplaceTypeCodeWithSubclasses.cpp
+++ Refactoring/ReplaceTypeCodeWithSubclasses.cpp
@@ -1,7 +1,53 @@
 class Employee {
-	int _type;
-	enum {ENGINEER = 0, SALESMAN = 1, MANAGER = 2};
 public:
-	Employee(int type): _type(type) {}
+	enum {ENGINEER = 0, SALESMAN = 1, MANAGER = 2};
+	static Employee *create(int type);
+	virtual ~Employee() {}
+	virtual int getType() const = 0;
 };
+
+class Engineer: public Employee {
+public:
+	virtual int getType(void) const {
+		return Employee::ENGINEER;
+	}
+};
+
+class Salesman: public Employee {
+public:
+	virtual int getType(void) const {
+		return Employee::SALESMAN;
+	}
+};
+
+class Manager: public Employee {
+public:
+	virtual int getType(void) const {
+		return Employee::MANAGER;
+	}
+};
+
+Employee *Employee::create(int type)
+{
+	Employee *employee = 0;
+
+	switch (type) {
+	case Employee::ENGINEER:
+		employee = new Engineer();
+		break;
+
+	case Employee::SALESMAN:
+		employee = new Salesman();
+		break;
+
+	case Employee::MANAGER:
+		employee = new Manager();
+		break;
+
+	default:
+		break;
+	}
+
+	return employee;
+}
 
