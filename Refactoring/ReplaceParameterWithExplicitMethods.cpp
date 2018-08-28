5c37006e6a6ca08ca804d896e57ddf4d48d06435
zhaokai115 <zhaokai115@gmail.com>
2018-08-28 03:09:39 -0400

    Refactoring: Replace Parameter with Explicit Methods - refactored

--- Refactoring/ReplaceParameterWithExplicitMethods.cpp
+++ Refactoring/ReplaceParameterWithExplicitMethods.cpp
@@ -5,41 +5,21 @@ public:
 	enum {ENGINEER = 0, SALESMAN = 1, MANAGER = 2};
 	Employee(int type): _type(type) {}
 
-	static Employee *create(int type);
-};
-
-class Engineer: public Employee {
-public:
-	Engineer(): Employee(ENGINEER) {}
-};
-
-class Salesman: public Employee {
-public:
-	Salesman(): Employee(SALESMAN) {}
-};
-
-class Manager: public Employee {
-public:
-	Manager(): Employee(MANAGER) {}
-};
-
-
-Employee *Employee::create(int type)
-{
-	switch (type) {
-	case ENGINEER:
-		return new Engineer();
-
-	case SALESMAN:
-		return new Salesman();
-
-	case MANAGER:
-		return new Manager();
+	static Employee *createEngineer() {
+		return new Employee(ENGINEER);
 	}
-}
+
+	static Employee *createSalesman() {
+		return new Employee(SALESMAN);
+	}
+
+	static Employee *createManager() {
+		return new Employee(MANAGER);
+	}
+};
 
 int main()
 {
-	Employee *kent = Employee::create(Employee::ENGINEER);
+	Employee *kent = Employee::createEngineer();
 }
 
