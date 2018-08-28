ce780be22dc914614c11761ce9a033c3fac4bb58
zhaokai115 <zhaokai115@gmail.com>
2018-08-28 02:34:38 -0400

    Refactoring: Parameterize Method - refactored

--- Refactoring/ParamterizeMethod.cpp
+++ Refactoring/ParamterizeMethod.cpp
@@ -1,17 +1,21 @@
-#include <algorithm>
+#include <limits.h>
 
 extern int lastUsage(void);
 extern int min(int, int);
 
+static int usageInRange(int lower, int upper)
+{
+	if (lastUsage() > lower)
+		return min(lastUsage(), upper) - lower;
+
+	return 0;
+}
+
 double baseCharge()
 {
-	double result = min(lastUsage(), 100) * 0.03;
-	if (lastUsage() > 100) {
-		result += (min(lastUsage(), 200) - 100) * 0.05;
-	}
-	if (lastUsage() > 200) {
-		result += (lastUsage() - 200) * 0.07;
-	}
+	double result = usageInRange(0, 100) * 0.03;
+	result += usageInRange(100, 200) * 0.05;
+	result += usageInRange(200, INT_MAX) * 0.07;
 
 	return result;
 }
