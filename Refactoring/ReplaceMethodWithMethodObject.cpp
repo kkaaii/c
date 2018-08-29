54cd92f5bd0ebcd7ecfef4b0101b61dbc54a12d0
zhaokai115 <zhaokai115@gmail.com>
2018-08-29 06:01:35 -0400

    Refactoring: Replace Method with Method Object - refactored

--- Refactoring/ReplaceMethodWithMethodObject.cpp
+++ Refactoring/ReplaceMethodWithMethodObject.cpp
@@ -1,13 +1,32 @@
-class Account {
+class Account;
+class Gamma {
+	const Account &_account;
+	int inputVal;
+	int quantity;
+	int yearToDate;
+	int importantValue1;
+	int importantValue2;
+	int importantValue3;
 public:
-	int gamma(int inputVal, int quantity, int yearToDate)
-	{
-		int importantValue1 = (inputVal * quantity) + delta();
+	Gamma(const Account &account, int inputValArg, int quantityArg, int yearToDateArg):
+		_account(account), inputVal(inputValArg), quantity(quantityArg), yearToDate(yearToDateArg) {
+		}
+	
+	int compute() {
+		int importantValue1 = (inputVal * quantity) + _account.delta();
 		int importantValue2 = (inputVal * yearToDate) + 100;
 		if ((yearToDate - importantValue1) > 100)
 			importantValue2 -= 20;
 		int importantValue3 = importantValue2 * 7;
 		// and so on
 		return importantValue3 - 2 * importantValue1;
+	}
+};
+
+class Account {
+public:
+	int gamma(int inputVal, int quantity, int yearToDate)
+	{
+		return Gamma(*this, inputVal, quantity, yearToDate).compute();
 	}
 };
