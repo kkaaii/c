8efd91b471f9e52e998595e4d77c0cf0f3fe3ef9
zhaokai115 <zhaokai115@gmail.com>
2018-08-29 07:33:08 -0400

    Refactoring:Move Method - refactored

--- Refactoring/MoveMethod.cpp
+++ Refactoring/MoveMethod.cpp
@@ -1,23 +1,32 @@
+class Account;
+
 class AccountType {
 public:
 	bool isPremium() const;
+	double overdraftCharge(const Account &account);
 };
 
 class Account {
 	AccountType _type;
 	int _daysOverdrawn;
 public:
-	double overdraftCharge() {
-		if (_type.isPremium()) {
-			double result = 10;
-			if (_daysOverdrawn > 7) result += (_daysOverdrawn - 7) * 0.85;
-			return result;
-		} else return _daysOverdrawn * 1.75;
-	}
-	
 	double bankCharge() {
 		double result = 4.5;
-		if (_daysOverdrawn > 0) result += overdraftCharge();
+		if (_daysOverdrawn > 0)
+			result += _type.overdraftCharge(*this);
 		return result;
 	}
+
+	int getDaysOverdrawn() const {
+		return _daysOverdrawn;
+	}
 };
+
+double AccountType::overdraftCharge(const Account &account) {
+	if (isPremium()) {
+		double result = 10;
+		if (account.getDaysOverdrawn() > 7)
+			result += (account.getDaysOverdrawn() - 7) * 0.85;
+		return result;
+	} else return account.getDaysOverdrawn() * 1.75;
+}
