2838b9fd98294853e9c71f32a7a3e17a9d39e660
zhaokai115 <zhaokai115@gmail.com>
2018-08-29 05:27:12 -0400

    Refactoring: Introduce Explaining Variable - refactored

--- Refactoring/IntroduceExplainingVariable.cpp
+++ Refactoring/IntroduceExplainingVariable.cpp
@@ -1,7 +1,7 @@
 double price()
 {
-	// price is base price - quantity discount + shipping
-	return _quantity * _itemPrice - 
-		max(0, _quantity - 500) * _itemPrice * 0.05 +
-		min(_quantity * _itemPrice * 0.1, 100.0);
+	const double basePrice = _quantity * _itemPrice;
+	const double quantityDiscount = max(0, _quantity - 500) * _itemPrice * 0.05;
+	const double shipping = min(basePrice * 0.1, 100.0);
+	return basePrice - quantityDiscount + shipping;
 }
