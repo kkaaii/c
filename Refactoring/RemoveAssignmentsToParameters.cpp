3ca037a9a8d761f333a1fa73d73e044925d1b4bd
zhaokai115 <zhaokai115@gmail.com>
2018-08-29 05:45:10 -0400

    Refactoring: Remove Assignments to Parameters - not refactored

---
+++ Refactoring/RemoveAssignmentsToParameters.cpp
@@ -0,0 +1,7 @@
+int discount(int inputVal, int quantity, int yearToDate)
+{
+	if (inputVal > 50) inputVal -= 2;
+	if (quantity > 100) inputVal -= 1;
+	if (yearToDate > 10000) inputVal -= 4;
+	return inputVal;
+}
