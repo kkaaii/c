7238e4ca6674f0d756bb474abe207d67e44a8056
zhaokai115 <zhaokai115@gmail.com>
2018-08-29 05:17:40 -0400

    Refactoring: Replace Temp with Query - refactored

--- Refactoring/ReplaceTempWithQuery.cpp
+++ Refactoring/ReplaceTempWithQuery.cpp
@@ -1,8 +1,14 @@
+int basePrice()
+{
+	return _quantity * _itemPrice;
+}
+
+double discountFactor()
+{
+	if (basePrice() > 1000) return 0.95;
+	else return 0.98;
+}
 double getPrice()
 {
-	int basePrice = _quantity * _itemPrice;
-	double discountFactor;
-	if (basePrice > 1000) discountFactor = 0.95;
-	else discountFactor = 0.98;
-	return basePrice * discountFactor;
+	return basePrice() * discountFactor();
 }
