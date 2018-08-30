8aef578fb9585e77375a7707ced12d41df7f405e
zhaokai115 <zhaokai115@gmail.com>
2018-08-30 07:23:02 -0400

    Refactoring: Duplicate Observed Data - refactored

--- Refactoring/DuplicateObservedData.cpp
+++ Refactoring/DuplicateObservedData.cpp
@@ -2,6 +2,36 @@
 
 typedef std::string String;
 
+class Interval: public Observable {
+	String _start;
+	String _end;
+	String _length;
+public:
+	String getStart() const {
+		return _start;
+	}
+
+	String getEnd() const {
+		return _end;
+	}
+
+	String getLength() const {
+		return _length;
+	}
+
+	void setStart(const String &text) {
+		_start = text;
+	}
+
+	void setEnd(const String &text) {
+		_end = text;
+	}
+
+	void setLength(const String &text) {
+		_length = text;
+	}
+};
+
 class TextField {
 public:
 	String getText() const;
@@ -9,39 +39,72 @@ public:
 };
 
 class IntervalWindow {
+	Interval *_subject;
 	TextField _startField;
 	TextField _endField;
 	TextField _lengthField;
+public:
+	IntervalWindow() {
+		_subject = new Interval();
+		_subject->addObserver(this);
+	}
 protected:
+	String getStart() const {
+		return _subject->getStart();
+	}
+
+	String getEnd() const {
+		return _subject->getEnd();
+	}
+
+	String getLength() const {
+		return _subject->getLength();
+	}
+
+	void setStart(String text) {
+		_subject->setStart(text);
+	}
+
+	void setEnd(String text) {
+		_subject->setEnd(text);
+	}
+
+	void setLength(String text) {
+		_subject->setLength(text);
+	}
+
 	void StartField_FocusLost() {
-		if (isNotInteger(_startField.getText()))
-			_startField.setText("0");
+		setStart(_startField.getText());
+		if (isNotInteger(getStart()))
+			setStart("0");
 		calculateLength();
 	}
 	
 	void EndField_FocusLost() {
-		if (isNotInteger(_endField.getText()))
-			_endField.setText("0");
+		setEnd(_endField.getText());
+		if (isNotInteger(getEnd()))
+			setEnd("0");
 		calculateLength();
 	}
 	
 	void LengthField_FocusLost() {
-		if (isNotInteger(_lengthField.getText()))
-			_lengthField.setText("0");
+		setLength(_lengthField.getText());
+		if (isNotInteger(getLength()))
+			setLength("0");
 		calculateEnd();
 	}
 	
 	void calculateLength() {
-		int start = StringToInt(_startField.getText());
-		int end = StringToInt(_endField.getText());
+		int start = StringToInt(getStart());
+		int end = StringToInt(getEnd());
 		int length = end - start;
-		_lengthField.setText(IntToString(length));
+		setLength(IntToString(length));
 	}
 	
 	void calculateEnd() {
-		int start = StringToInt(_startField.getText());
-		int length = StringToInt(_lengthField.getText());
+		int start = StringToInt(getStart());
+		int length = StringToInt(getLength());
 		int end = start + length;
-		_endField.setText(IntToString(end));
+		setEnd(IntToString(end));
 	}
 };
