d65b8f3f98e783774eb79166ab5350962eb7399b
zhaokai115 <zhaokai115@gmail.com>
2018-08-30 07:39:33 -0400

    Refactoring: Change Unidirectional Association to Bidirectional - refactored

--- Refactoring/ChangeUnidirectionalAssociationToBidirectional.cpp
+++ Refactoring/ChangeUnidirectionalAssociationToBidirectional.cpp
@@ -9,13 +9,26 @@ public:
 		return _customer;
 	}
 
-	void setCustomer(Customer *customer) {
-		_customer = customer;
-	}
+	void setCustomer(Customer *customer);
 };
 
 typedef std::set<Order *> Orders;
 
 class Customer {
 	Orders	_orders;
+public:
+	void add(Order *order) {
+		_orders.add(order);
+	}
+
+	void remove(Order *order) {
+		_orders.remove(order);
+	}
 };
+
+void Order::setCustomer(Customer *customer)
+{
+	if (!_customer) _customer.remove(this);
+	_customer = customer;
+	if (!_customer) _customer.add(this);
+}
