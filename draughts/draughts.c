a275c58b318ab629661cf0f7e6efca1c3dd5e9e7
kkaaii <zhaokai115@163.com>
2019-12-02 21:01:12 +0800

    Update draughts.c
    Helper function

--- draughts/draughts.c
+++ draughts/draughts.c
@@ -5,6 +5,8 @@
 #define NTEAM   2
 #define NP      12
 
+#define MAX_NPATH   10
+
 #define PID_NIL 0
 
 typedef char    PID;    /* piece identifier */
@@ -41,12 +43,52 @@ typedef struct team {
     char    row_king;
 } TEAM;
 
-typedef struct step {
+typedef struct pos {
     char    row;
     char    col;
-} STEP;
+} POS;
 
 typedef struct path {
     char    nstep;
-    STEP    steps[1 + NP];
+    POS     steps[1 + NP];
 } PATH;
+
+const DIR dirs[] = {{-1, -1}, {-1, 1}, {1, 1}, {1, -1}};
+
+PID board[NROW][NCOL];
+struct piece pieces[1 + NTEAM * NP];
+TEAM teams[1 + NTEAM];
+
+TID turn = eDark;
+
+char npath = 0;
+PATH longest[MAX_NPATH];
+PATH current;
+
+static inline BOOL is_in_board(char row, char col)
+{
+    return 0 <= row && row < NROW && 0 <= col && col < NCOL;
+}
+
+static inline PID get_first(TID tid)
+{
+    return teams[tid].head;
+}
+
+static inline PID get_next(PID pid)
+{
+    return pieces[pid].next;
+}
+
+static inline void path_add_step(PATH *path, char row, char col)
+{
+    POS *step = &path->steps[path->nstep++];
+    
+    step->row = row;
+    step->col = col;
+}
+
+static inline void path_del_last(PATH *path)
+{
+    --path->nstep;
+}
