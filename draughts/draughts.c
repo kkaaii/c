08100cc9b800a04e100999dc57917c518a15e9b9
kkaaii <zhaokai115@163.com>
2019-12-08 11:03:32 +0800

    Update draughts.c
    Add operations

--- draughts/draughts.c
+++ draughts/draughts.c
@@ -83,12 +83,105 @@ static inline PID get_next(PID pid)
 static inline void path_add_step(PATH *path, char row, char col)
 {
     POS *step = &path->steps[path->nstep++];
-    
+
     step->row = row;
     step->col = col;
+}
+
+static inline void path_add_move(char row_orig, char col_orig, char row_new, char col_new)
+{
+    PATH *path = &longest[npath++];
+
+    path->steps[0].row = row_orig;
+    path->steps[0].col = col_orig;
+
+    path->steps[1].row = row_new;
+    path->steps[1].col = col_new;
+
+    path->nstep = 2;
 }
 
 static inline void path_del_last(PATH *path)
 {
     --path->nstep;
+}
+
+void update_longest_path(void)
+{
+    if (current.nstep < longest[0].nstep)
+        return;
+
+    if (current.nstep > longest[0].nstep)
+        npath = 0;
+
+    if (npath < MAX_NPATH)
+        memcpy(&longest[npath++], &current, sizeof (PATH));
+}
+
+static inline void drop(PID pid, char row, char col)
+{
+    struct piece *piece = &pieces[pid];
+
+    piece->row = row;
+    piece->col = col;
+    board[row][col] = pid;
+}
+
+static inline void crown(PID pid)
+{
+    struct piece *piece = &pieces[pid];
+
+    if (piece->row == teams[piece->tid].row_king)
+        piece->king = TRUE;
+}
+
+static void kill(PID pid)
+{
+    struct piece *piece = &pieces[pid];
+    PID prev = piece->prev;
+    PID next = piece->next;
+    char row = piece->row;
+    char col = piece->col;
+
+    board[row][col] = PID_NIL;
+
+    if (PID_NIL != prev)
+        pieces[prev].next = next;
+    else
+        teams[piece->tid].head = next;
+
+    if (PID_NIL != next)
+        pieces[next].prev = prev;
+}
+
+void jump(PATH *path)
+{
+    char row = path->steps[0].row;
+    char col = path->steps[0].col;
+    PID  pid = board[row][col];
+    POS *step;
+
+    board[row][col] = PID_NIL;
+    for (step = &path->steps[1]; step < &path->steps[path->nstep]; ++step) {
+        kill(board[(row + step->row) / 2][(col + step->col) / 2]);
+        row = step->row;
+        col = step->col;
+    }
+
+    drop(pid, row, col);
+    crown(pid);
+}
+
+void move(PATH *path)
+{
+    char row = path->steps[0].row;
+    char col = path->steps[0].col;
+    PID  pid = board[row][col];
+
+    board[row][col] = PID_NIL;
+
+    row = path->steps[1].row;
+    col = path->steps[1].col;
+    drop(pid, row, col);
+    crown(pid);
 }
