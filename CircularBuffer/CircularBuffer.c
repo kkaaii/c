d715b9038e1e1e376d3bb816714f03590a68a711
kkaaii <zhaokai115@163.com>
2020-03-06 05:38:32 +0000

    add CircularBuffer_IsFull()

--- CircularBuffer/CircularBuffer.c
+++ CircularBuffer/CircularBuffer.c
@@ -1,11 +1,47 @@
+#include <stdlib.h>
+
 typedef struct {
-	int	head;
-	int	tail;
+	unsigned int	head;
+	unsigned int	tail;
+	unsigned int	mask;
+	int	*data;
 } CircularBuffer;
+
+CircularBuffer *CircularBuffer_Create(unsigned int count)
+{
+	CircularBuffer *self = NULL;
+	size_t size;
+
+	/* count must be power of 2 */
+	if (0 == (count & (count - 1))) {
+		size = sizeof (CircularBuffer) + count * sizeof (int);
+		self = (CircularBuffer *)malloc(size);
+		if (NULL != self) {
+			self->head = self->tail = 0;
+			self->mask = count - 1;
+			self->data = (int *)(self + 1);
+		}
+	}
+
+	return self;
+}
+
+void CircularBuffer_Destroy(CircularBuffer *self)
+{
+	if (NULL != self) {
+		free(self);
+		self = NULL;
+	}
+}
 
 bool CircularBuffer_IsEmpty(CircularBuffer *self)
 {
 	return self->tail == self->head;
+}
+
+bool CircularBuffer_IsFull(CircularBuffer *self)
+{
+	return self->tail - self->head == self->mask + 1;
 }
 
 void CircularBuffer_Put(CircularBuffer *self, int value __attribute__((unused)))
--- CircularBuffer/cpputest/CircularBuffer_Test.cpp
+++ CircularBuffer/cpputest/CircularBuffer_Test.cpp
@@ -4,35 +4,55 @@ extern "C" {
 #include "../CircularBuffer.c"
 }
 
+#define	BUF_SIZE	8
+
 TEST_GROUP(CircularBuffer)
 {
 	void setup() {
-		buffer = new CircularBuffer;
+		self = CircularBuffer_Create(BUF_SIZE);
 	}
 
 	void teardown() {
-		delete buffer;
+		CircularBuffer_Destroy(self);
 	}
 
-	CircularBuffer	*buffer;
+	CircularBuffer	*self;
 };
 
 TEST(CircularBuffer, empty_after_creation)
 {
-	CHECK_TRUE(CircularBuffer_IsEmpty(buffer));
+	CHECK_TRUE(CircularBuffer_IsEmpty(self));
+	CHECK_FALSE(CircularBuffer_IsFull(self));
 }
 
 TEST(CircularBuffer, not_empty_after_put)
 {
-	CircularBuffer_Put(buffer, 42);
-	CHECK_FALSE(CircularBuffer_IsEmpty(buffer));
+	CircularBuffer_Put(self, 42);
+	CHECK_FALSE(CircularBuffer_IsEmpty(self));
 }
 
 TEST(CircularBuffer, empty_after_remove_the_last_item)
 {
-	CircularBuffer_Put(buffer, 42);
-	CHECK_FALSE(CircularBuffer_IsEmpty(buffer));
-	CircularBuffer_Get(buffer);
-	CHECK_TRUE(CircularBuffer_IsEmpty(buffer));
+	CircularBuffer_Put(self, 42);
+	CHECK_FALSE(CircularBuffer_IsEmpty(self));
+	CircularBuffer_Get(self);
+	CHECK_TRUE(CircularBuffer_IsEmpty(self));
 }
 
+TEST(CircularBuffer, put_until_full)
+{
+	for (int i = 0; i < BUF_SIZE; ++i) {
+		CHECK_FALSE(CircularBuffer_IsFull(self));
+		CircularBuffer_Put(self, 42);
+	}
+	CHECK_TRUE(CircularBuffer_IsFull(self));
+}
+
+TEST(CircularBuffer, not_full_after_remove_one_item)
+{
+	for (int i = 0; i < BUF_SIZE; ++i) {
+		CircularBuffer_Put(self, 42);
+	}
+	CircularBuffer_Get(self);
+	CHECK_FALSE(CircularBuffer_IsFull(self));
+}
